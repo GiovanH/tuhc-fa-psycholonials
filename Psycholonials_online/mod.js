9ae3eccb4a8858e14bcdd72ffdc60069816f4bed
Gio <sethg@ipi.org>
2024-06-07 19:35:11 -0500

    Initial commit existing mod

---
+++ Psycholonials_online/mod.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "Psycholonials (online)",
+  summary: "MSPFA",
+  
+  edit: true,
+
+  trees: {
+    './': 'assets://mspfa/Psycholonials_online/',
+  },
+  async asyncComputed(api) {
+    const story = await api.readYamlAsync("./story.yaml")
+    return {
+      styles: [
+        {body: await api.readFileAsync("./adventure.scss")}
+      ],
+      edit(archive){
+        archive.mspfa['Psycholonials_online'] = story
+      }
+    }
+  }
+}
