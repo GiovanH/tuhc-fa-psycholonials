5c15d923724e22d7c2f6d6cc76b2cc2933c70d6f
Gio <sethg@ipi.org>
2024-06-07 21:21:33 -0500

    Add full adventure

---
+++ Psycholonials/mod.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: "Psycholonials",
+  author: "Gio",
+  summary: "LP Psycholonials MSPFA",
+  version: 1.0,
+
+  edit: true,
+
+  trees: {
+    './': 'assets://mspfa/Psycholonials_online/',
+  },
+  async asyncComputed(api) {
+    const story = await api.readYamlAsync("./story.yaml")
+    return {
+      styles: [
+        {body: await api.readFileAsync("./adventure.scss")}
+      ],
+      edit(archive){
+        archive.mspfa['Psycholonials_online'] = story
+      }
+    }
+  }
+}
+
